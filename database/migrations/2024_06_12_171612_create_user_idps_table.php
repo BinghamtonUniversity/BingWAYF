4c730608a0c470feba80d92afefa241cc33f5ffc
Tim Cortesi <tcortesi@gmail.com>
2024-08-29 16:41:10 -0400

    Add type to user_idps table

--- database/migrations/2024_06_12_171612_create_user_idps_table.php
+++ database/migrations/2024_06_12_171612_create_user_idps_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_idps', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->index();
+            $table->string('type',20)->nullable()->default(null)->index();
             $table->unsignedBigInteger('idp_id')->nullable()->index();
             $table->string('unique_id')->index();
             $table->json('attributes')->nullable()->default(null);
